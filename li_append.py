9e52333db0952b4eda1dfdf2d50c1d14a5dff5c6
Yeabsiira <yeabsira.dessalegn@aastustudent.edu.et>
2025-04-05 20:46:07 +0200

    list

---
+++ li_append.py
@@ -0,0 +1,9 @@
+# Creating an empty list
+names = []
+
+# Appending items to the list
+names.append("Abebe")
+names.append("Chala")
+names.append("Dametew")
+
+print(names)  # Output: ['apple', 'banana', 'cherry']
---
+++ li_len.py
@@ -0,0 +1,7 @@
+# Creating a list of numbers
+names = ['Abebe','Chala', 'Chaltu', 'Damtew', 'Yonas']
+
+# Finding the length of the list
+length_of_list = len(names)
+
+print(length_of_list)  # Output: 5
